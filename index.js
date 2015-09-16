9abffc090be11ebdf7995eadc9d03455dfa177ac
Perry Poon <plrthink@gmail.com>
2015-09-17 00:03:08 +0800

    make download task  async

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 'use strict'
 
 var RNFileDownload = require('NativeModules').RNFileDownload
+var Promise = require('bluebird')
+
+var _download = Promise.promisify(RNFileDownload.download);
+
+var _error = (err) => {
+  throw error
+}
 
 var FileDownload = {
-  download: function(source, target, callback) {
-    RNFileDownload.download(source, target, callback)
+  download(source, target) {
+    return _download(source, target)
+      .catch(_error)
   }
 }
 
