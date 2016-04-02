08b30b670056dfae86f1f9c0da78e6928da9fc4e
Luavis <luaviskang@gmail.com>
2016-04-02 20:16:49 +0900

    Fix ownership attribute

--- RNFileDownloadSessionManager.h
+++ RNFileDownloadSessionManager.h
@@ -14,10 +14,10 @@
 #define RANDOM_FILENAME_LENGTH 10
 
 @interface RNFileDownloadSessionManager : NSObject<NSURLSessionDownloadDelegate>
-@property (nonatomic) RCTResponseSenderBlock callback;
-@property (nonatomic) RCTBridge *bridge;
-@property (nonatomic, readonly) NSString *targetPath;
-@property (nonatomic, readonly) NSString *downloadFileName;
+@property (nonatomic, strong) RCTResponseSenderBlock callback;
+@property (nonatomic, strong) RCTBridge *bridge;
+@property (nonatomic, strong, readonly) NSString *targetPath;
+@property (nonatomic, strong, readonly) NSString *downloadFileName;
 
 - (instancetype)initWithTargetPath:(NSString *)targetPath downloadFileName:(NSString *)fileName bridge:(RCTBridge *)bridge callback:(RCTResponseSenderBlock)callback;
 
